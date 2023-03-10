2cf4f8bb3bbc711fbcc73e67d2d14ee947af33f4
Mickeal Di Curzio <mdicurzio@kaliop.com>
2023-03-10 10:43:00 +0100

    feat(experiences): start view full

--- src/components/GlobalHero/GlobalHero.vue
+++ src/components/GlobalHero/GlobalHero.vue
@@ -1,16 +1,39 @@
 <template>
-  <v-parallax
-      height="500px"
-      width="50%"
-      src="https://img.freepik.com/premium-photo/always-state-wonder-awe_73899-12.jpg?w=1380"
-  ></v-parallax>
+  <div class="GlobalHero">
+    <div class="GlobalHero-container">
+      <v-parallax
+          class="GlobalHero-parallax"
+          height="600px"
+          src="https://img.freepik.com/premium-photo/always-state-wonder-awe_73899-12.jpg?w=1380"
+      >
+        <div class="GlobalHero-title">
+          <h1>{{ heroTitle }}</h1>
+        </div>
+      </v-parallax>
+      <div class="GlobalHero-left"
+           :class="backgroundColor !== '#fff' ? 'blankFill' : ''"
+           :style="{backgroundColor: backgroundColor}">
+        <v-img :src="leftImageSrc" class="GlobalHero-leftImage"></v-img>
+        <div class="GlobalHero-leftBefore" :style="{backgroundColor: backgroundColor}">
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import './GlobalHero.scss'
+
 export default {
-  name: "Hero",
+  name: "GlobalHero",
   props: {
-    imageSrc: String
+    imageSrc: String,
+    backgroundColor: {
+      type: String,
+      default: "#fff"
+    },
+    heroTitle: String,
+    leftImageSrc: String
   }
 }
 </script>
---
+++ src/views/Experiences/ExperiencesFull.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="ExperiencesFull">
+    <div class="ExperiencesHero">
+        <global-hero
+            image-src="https://img.freepik.com/premium-photo/always-state-wonder-awe_73899-12.jpg?w=1380"
+            hero-title="Experiences"
+            :left-image-src="Pandules"
+        ></global-hero>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Pandules from "@/assets/images/pendule.svg";
+
+import GlobalHero from "@/components/GlobalHero/GlobalHero";
+
+export default {
+  name: "ExperiencesFull",
+  components: {
+    GlobalHero,
+  },
+  setup() {
+    return {
+      Pandules,
+    };
+  },
+}
+</script>
